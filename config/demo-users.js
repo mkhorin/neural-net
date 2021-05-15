5b5a1dccc39f5c4815c120df8f7160688c3b005b
mkhorin <maksimovichu@gmail.com>
2021-05-15 11:23:15 +1000

    Add demo configuration

---
+++ config/demo-users.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = [{
+    name: 'Adam',
+    email: 'a@a.a',
+    passwordHash: '0a307ffa1284b4ac14cd951a3bb85bf776b0ddfb70e065f92d5eea9133b92c5ddede094a',
+    verified: true
+}];
---
+++ config/demo.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+
+    parent: 'default',
+    port: 3000,
+
+    params: {
+        'static': {
+            options: {
+                maxAge: 8 * 60 * 60 * 1000
+            }
+        }
+    },
+    users: require('../../neural/config/demo-users')
+};
